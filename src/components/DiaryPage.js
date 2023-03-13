0ee2d156c7680c97475cca6c29b6be3fe110bd78
Yuni0221 <moonrise002002@gmail.com>
2023-03-13 16:41:40 +0900

    Diary 구현

--- src/components/DiaryPage.js
+++ src/components/DiaryPage.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./DiaryPage.module.css";
+import { dbService } from "../Firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+import MainPage from "./MainPage";
+import { set } from "date-fns";
 
 function DiaryPage() {
-  return <div>This is Diary Page</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onDiaryChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onDiarySubmit = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(dbService, "dailymung-diary"), {
+      title,
+      content,
+    });
+  };
+  return (
+    <>
+      <div>
+        <MainPage />
+      </div>
+      <form className={styles.diaryForm} onSubmit={onDiarySubmit}>
+        <input type="text" placeholder="Title" onChange={onDiaryChange}></input>
+        <input
+          type="textarea"
+          placeholder="Record you day"
+          onChange={onContentChange}
+        ></input>
+        <input type="submit" value="submit"></input>
+      </form>
+    </>
+  );
 }
 
 export default DiaryPage;
