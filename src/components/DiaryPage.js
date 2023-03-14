1b12811a486400da8a47351c7f951c29b837b799
Yuni0221 <moonrise002002@gmail.com>
2023-03-14 12:43:46 +0900

    Diary Page 구현

--- src/components/DiaryPage.js
+++ src/components/DiaryPage.js
@@ -1,46 +1,69 @@
 import React, { useState, useEffect } from "react";
 import styles from "./DiaryPage.module.css";
 import { dbService } from "../Firebase";
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
 
 import MainPage from "./MainPage";
-import { set } from "date-fns";
 
-function DiaryPage() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+function DiaryPage({ userObj }) {
+  const [title, setTitle] = useState();
+  const [getTitle, setGetTitle] = useState([]);
 
-  const onDiaryChange = (e) => {
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "dailymung-diary"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const diaryArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      // setGetTitle(diaryArr);
+      console.log(diaryArr);
+    });
+  });
+
+  const onTitleChange = (e) => {
+    e.preventDefault();
     setTitle(e.target.value);
-  };
-
-  const onContentChange = (e) => {
-    setContent(e.target.value);
   };
 
   const onDiarySubmit = async (e) => {
     e.preventDefault();
     await addDoc(collection(dbService, "dailymung-diary"), {
       title,
-      content,
     });
   };
+
   return (
     <>
       <div>
         <MainPage />
       </div>
       <form className={styles.diaryForm} onSubmit={onDiarySubmit}>
-        <input type="text" placeholder="Title" onChange={onDiaryChange}></input>
         <input
-          type="textarea"
-          placeholder="Record you day"
-          onChange={onContentChange}
+          className={styles.title}
+          type="text"
+          placeholder="Title"
+          onChange={onTitleChange}
         ></input>
-        <input type="submit" value="submit"></input>
+
+        <input className={styles.button} type="submit" value="Submit"></input>
       </form>
+
+      <div>
+        {getTitle.map((title) => (
+          <h4>{title}</h4>
+        ))}
+      </div>
     </>
   );
 }
-
 export default DiaryPage;
