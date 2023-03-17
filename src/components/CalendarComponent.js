52c58ae60768faff3ae7d0ff375467992cc2ab95
Yuni0221 <moonrise002002@gmail.com>
2023-03-17 17:58:27 +0900

    Calendar페이지 수정

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-// import styles from "./CalendarComponent.module.css";
+
+import styles from "./CalendarComponent.module.css";
+import moment from "moment";
 
 function CalendarComponent() {
   const [date, setDate] = useState(new Date());
 
   return (
     <>
-      <Calendar onChange={setDate} value={date} />
-      <div>Selected date : {date.toDateString()} </div>
+      <Calendar
+        className={styles.calendar}
+        onChange={setDate}
+        value={date}
+        formatDay={(locale, date) => moment(date).format("DD")}
+      />
+      <div className={styles.selectDate}>
+        Selected date : {date.toDateString()}{" "}
+      </div>
     </>
   );
 }
